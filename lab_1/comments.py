d843d1a69f63b2bf18a0dc8100100df3f797a824
Sultan <sultanbeimaganbet@gmail.com>
2025-01-19 13:04:25 +0500

    bardi ma

---
+++ lab_1/comments.py
@@ -0,0 +1,8 @@
+"""
+this is
+a comment
+written in
+more thaan just one line    
+"""
+
+print("hello world!")
---
+++ lab_1/variables.py
@@ -0,0 +1,39 @@
+#1
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#2
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#3
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#4
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#5
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+#6
