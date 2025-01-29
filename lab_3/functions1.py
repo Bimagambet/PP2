46c94ef4ff29d919a8d0ff20966afed25574848b
Sultan <sultanbeimaganbet@gmail.com>
2025-01-29 10:15:57 +0500

    bardy ma

---
+++ lab_3/functions1.py
@@ -0,0 +1,6 @@
+# task-1
+def grams_to_ounces(gramdar):
+    return gramdar * 28.3495231
+
+gramdar = float(input("gramdar jaz"))
+print(f"{grams_to_ounces(gramdar)} ounces")
--- lab_3/tuples.py
+++ lab_3/tuples.py
@@ -1,1 +1,10 @@
-print("hello gays")
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
