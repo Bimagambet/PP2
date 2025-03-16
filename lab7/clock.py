55fcc98c8119b8057bcb72e48438632b26931ea1
Sultan <sultanbeimaganbet@gmail.com>
2025-03-17 01:05:05 +0500

    labb

--- lab7/clock.py
+++ lab7/clock.py
@@ -6,4 +6,37 @@ pygame.init()
 w = 800
 d = 600
 
-clock = pygame.time.clock()
+pygame.display.set_caption("clock!")
+
+clock = pygame.time.Clock()
+screen =  pygame.display.set_mode((w, d))
+bg = pygame.image.load("clock.png")
+
+min_hand = pygame.image.load("min_hand.png")
+sec_hand = pygame.image.load("sec_hand.png")
+
+def rotate(surf, img, times, angle):
+    rot_img = pygame.transform.rotate(img, - (times % 60) * 6 + angle)
+    new_img = rot_img.get_rect(center = img.get_rect(center = (400, 300)).center)
+    print(new_img)
+    surf.blit(rot_img, new_img)
+    
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    curtime = datetime.datetime.now()
+    minuts = curtime.minute
+    seconds = curtime.second
+    
+    screen.fill((0, 0, 0))
+    
+    screen.blit(bg, (0,0))
+    
+    rotate(screen, sec_hand, seconds, 60)
+    rotate(screen, min_hand, minuts, -45)
+    
+    pygame.display.update()
+    clock.tick(60)
